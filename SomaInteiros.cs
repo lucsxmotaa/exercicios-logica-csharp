ffaa89399f5ef03a3071960bb24123d6ac3f2fec
Lucas Mota <xyxmotaa@gmail.com>
2026-01-22 10:24:09 -0300

    Adicionando o segundo exercicio de lógica

---
+++ SomaInteiros.cs
@@ -0,0 +1,15 @@
+using System;
+class SomaInteiros
+{
+    static void Main (String [] args)
+    {
+        int Valor1, Valor2, Soma;
+
+        Valor1 = 24;
+        Valor2 = 41;
+
+        Soma = Valor1 + Valor2;
+
+        Console.WriteLine(Soma);
+    }
+}
