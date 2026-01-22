d6192247734f507def471351968257c7eeeff383
Lucas Mota <xyxmotaa@gmail.com>
2026-01-22 10:13:41 -0300

    Adicionando o primeiro exercício

---
+++ OlaMundo.cs
@@ -0,0 +1,13 @@
+using System;
+class OlaMundo
+{
+    static void Main()
+    {
+        Console.WriteLine("Olá Mundo!");
+    }
+}
+
+// Beleza o código tá rodando, mas porque ele roda sem o (String[] args)?
+
+// Em C#, o método Main pode ser definido de várias maneiras, incluindo com ou sem parâmetros.
+// Quando você define o Main sem parâmetros, ele simplesmente não espera nenhum argumento de linha de comando
