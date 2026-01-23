1ef7ab22667b744d713852178f2febca4f06e2be
Lucas Mota <xyxmotaa@gmail.com>
2026-01-23 10:39:02 -0300

    Adicionando o quarto exercicio de lógica

---
+++ AreaRetangulo.cs
@@ -0,0 +1,13 @@
+using System;
+class AreaRetangulo
+{
+    static void Main (String [] args)
+    {
+        int Base, altura, area;
+        Base = 14;
+        altura = 8;
+        area = Base * altura;
+        Console.WriteLine("A area do retangulo e: " + area);
+    }
+
+}
